8ed83af1e72baa98796436e18d547863c0971d9e
ornao <reynoldsorna@gmail.com>
2023-09-05 20:48:27 +0000

    add cache pollicy for static files

--- vintage/settings.py
+++ vintage/settings.py
@@ -130,6 +130,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'vintagepp5'
     AWS_S3_REGION_NAME = 'eu-west-1'
