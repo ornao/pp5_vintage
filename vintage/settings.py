108aaa6cadd93b50fe5320e191aaa813da40325d
ornao <reynoldsorna@gmail.com>
2023-09-09 12:26:00 +0000

    add framework for css, html and bootstrap code

--- vintage/settings.py
+++ vintage/settings.py
@@ -156,6 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 if 'USE_AWS' in os.environ:
     # Cache control
