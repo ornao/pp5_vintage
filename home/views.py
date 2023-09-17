f91cc23ded4cd132935e9ef18313d5375ff176fe
ornao <reynoldsorna@gmail.com>
2023-09-17 19:33:08 +0000

    add js links to get messages toasts to display

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
-    """ A view to return the index page """
+    """ A view to return the index page and sucessful login message """
 
     return render(request, 'home/index.html')
