9660e27e80a5b603cdbb2096974c915a9b99cd58
ornao <reynoldsorna@gmail.com>
2023-09-25 15:19:48 +0000

    add core checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,42 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-import os
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': os.environ.get('STRIPE_KEY', 'pk_test_51NgWZzLHAE7MTuBZ36qHXhMYWe1co9lOaTxk9dT56RYoVYwg7zI90iTVU9P9gCNSxmG7L1iUhCl0YwwxPqAZ2b0f005ASF8Eeb'),
-        'stripe_client_secret': os.environ.get('STRIPE_SECRET', ''),
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
--- vintage/settings.py
+++ vintage/settings.py
@@ -195,8 +195,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET', '')
 
 if 'USE_AWS' in os.environ:
     # Cache control
