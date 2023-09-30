1eb166b04fd2cdf66216edf649f9df19fa3d5301
ornao <reynoldsorna@gmail.com>
2023-09-30 12:21:18 +0000

    add profile form, views and updated templates

--- vintage/settings.py
+++ vintage/settings.py
@@ -239,7 +239,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'timesgoneby@example.com'
+    DEFAULT_FROM_EMAIL = 'preloved@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
