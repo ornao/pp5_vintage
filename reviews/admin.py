77837e64eaa97616e36fc4d3b668529272fb7f66
ornao <reynoldsorna@gmail.com>
2023-10-02 19:11:23 +0000

    attempt fix for product category error

--- reviews/admin.py
+++ reviews/admin.py
@@ -4,7 +4,7 @@ from .models import Reviews
 # Register your models here.
 
 class ReviewsAdmin(admin.ModelAdmin):
-    list_display = ('reviews_id', 'rating', 'status', 'created_date')
+    list_display = ('rating', 'status', 'created_date')
     search_fields = ['rating']
     list_filter = ('status', 'created_date')
 
