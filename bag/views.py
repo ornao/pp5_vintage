98555ad23ad73b6eddbfdfc35993b487bbe7eb33
ornao <reynoldsorna@gmail.com>
2023-10-02 19:31:14 +0000

    reset database

--- bag/views.py
+++ bag/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect, reverse
 def view_bag(request):
     """ A view that renders the bag contents page """
 
-    return render(request, 'templates/bag/bag.html')
+    return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
